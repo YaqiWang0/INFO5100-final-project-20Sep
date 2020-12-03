e82fb8c422052bde61078aa0a29c3186bd28c652
Anjali-Chitkara-ac <69366199+Anjali-Chitkara-ac@users.noreply.github.com>
2020-12-02 19:14:06 -0800

    minor changes

--- src/incentive/CreateIncentive.java
+++ src/incentive/CreateIncentive.java
@@ -1,19 +1,52 @@
 package incentive;
 
+import org.jdatepicker.DateModel;
+import org.jdatepicker.JDatePicker;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CreateIncentive {
+public class CreateIncentive extends JFrame {
     private JButton button1;
     private JPanel panelMain;
     private JTabbedPane tabbedPane1;
     private JPanel Value;
     private JPanel Details;
     private JPanel Description;
+    private JComboBox startMonth;
+    private JComboBox startDay;
+    private JComboBox startYear;
 
     public CreateIncentive() {
+        initComponents();
+    }
+
+    private void initComponents(){
+        publish();
+        addItemsToComboBoxes();
+
+    }
+
+    public void addItemsToComboBoxes(){
+        //add items for start month combo box
+        for(int i=1;i<=12;i++){
+            startMonth.addItem(i);
+        }
+
+        //add items for start day combo box
+        for(int i = 1;i<=31;i++){
+            startDay.addItem(i);
+            //filter as/month
+        }
+
+        for(int i = 2020;i<2050;i++){
+            startYear.addItem(i);
+        }
+    }
+
+    public void publish(){
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -23,8 +56,9 @@ public class CreateIncentive {
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Create Incentive");
-        frame.setContentPane(new CreateIncentive().panelMain);
+        CreateIncentive frame = new CreateIncentive();
+        frame.setTitle("Create Incentive");
+        frame.setContentPane(frame.panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(400, 300));
         frame.pack();
