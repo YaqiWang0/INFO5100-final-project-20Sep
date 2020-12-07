6f9ef8ac9fa9c00f121974674e2bb65081ebce41
TheKingHusky <60021689+TheKingHusky@users.noreply.github.com>
2020-12-07 07:34:39 -0800

    Created UI for Inventory Update and added action listeners for some buttons

--- src/ui/InventoryManagementJPanel.java
+++ src/ui/InventoryManagementJPanel.java
@@ -3,6 +3,9 @@ package ui;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InventoryManagementJPanel {
     private JPanel InventoryMgmtPanel;
@@ -10,7 +13,7 @@ public class InventoryManagementJPanel {
     private JTextField searchTextField;
     private JButton allButton;
     private JButton searchButton;
-    private JTable table1;
+    public JTable table1;
     private JButton createButton;
     private JButton updateButton;
     private JButton deleteButton;
@@ -30,12 +33,57 @@ public class InventoryManagementJPanel {
         frame.setVisible(true);
 
         String[] columnNames = {"ID", "Dealer ID", "Year", "Brand", "Model", "New/Used", "Price", "Exterior Color", "Interior Color", "Body Type", "Features", "Miles", "Images"};
-        String[][] tableVales = {{"ID", "Dealer ID", "Year", "Brand", "Model", "New/Used", "Price", "Exterior Color", "Interior Color", "Body Type", "Features", "Miles", "Images"}};
+        String[][] tableVales = {{"1", "Dealer ID", "Year", "Brand1", "Model", "New/Used", "Price", "Exterior Color", "Interior Color", "Body Type", "Features", "Miles", "Images"},{"2", "Dealer ID", "Year", "Brand2", "Model", "New/Used", "Price", "Exterior Color", "Interior Color", "Body Type", "Features", "Miles", "Images"},{"3", "Dealer ID", "Year", "Brand2", "Model", "New/Used", "Price", "Exterior Color", "Interior Color", "Body Type", "Features", "Miles", "Images"}};
         TableModel tableModel = new DefaultTableModel(tableVales, columnNames);
         table1.setModel(tableModel);
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String searchContent = searchTextField.getText();
+                TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(((DefaultTableModel) table1.getModel()));
+                sorter.setRowFilter(RowFilter.regexFilter(searchTextField.getText()));
+
+                table1.setRowSorter(sorter);
+            }
+        });
+        allButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(((DefaultTableModel) table1.getModel()));
+                sorter.setRowFilter(RowFilter.regexFilter(""));
+
+                table1.setRowSorter(sorter);
+            }
+        });
+
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = table1.getSelectedRow();
+                InventoryUpdateJPanel panel = new InventoryUpdateJPanel(table1.getValueAt(row, 0).toString(), table1.getValueAt(row, 1).toString());
+                System.out.println(table1.getValueAt(row, 0).toString() + " " + table1.getValueAt(row, 1).toString());
+            }
+        });
+
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InventoryCreationJPanel panel = new InventoryCreationJPanel();
+            }
+        });
+
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                table1.remove(table1.getSelectedRow());
+            }
+        });
     }
 
     public static void main(String[] args){
         InventoryManagementJPanel im = new InventoryManagementJPanel();
     }
+
+
 }
