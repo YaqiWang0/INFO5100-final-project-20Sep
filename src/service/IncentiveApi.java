ab31946d74bf517220379c97a3efbfa56a6fd98d
Eli-YuZhang <71057255+Eli-YuZhang@users.noreply.github.com>
2020-12-14 13:05:01 +0800

    vm to sm

--- src/service/IncentiveApi.java
+++ src/service/IncentiveApi.java
@@ -1,12 +1,12 @@
 package service;
 
+import dao.SpecialModel;
 import dao.Vehicle;
-import dao.VehicleModel;
 
 import java.util.Date;
 
 public interface IncentiveApi {
 
-    VehicleModel updateSpecialPrice(Vehicle vehicle);
+	SpecialModel updateSpecialPrice(Vehicle vehicle);
 
 }
